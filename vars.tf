e2563519ec8bd731d62a8e4cbe94fa1b0277d4c4
s poorna chandra <poornanaidu1@gmail.com>
2025-01-22 12:17:14 +0530

    dev environment

---
+++ vars.tf
@@ -0,0 +1,6 @@
+variable "vpc" {}     #this is variable for module in main.tf in roboshop-terraform-v1 and we are sending to main.tf module
+variable "default_vpc_id" {}
+variable "default_vpc_cidr" {}
+variable "default_vpc_route_table_id" {}
+variable "tags" {}
+variable "env" {}
