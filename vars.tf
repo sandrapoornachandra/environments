3bcc39040ded2c4645fc3781a2812096885a8b21
s poorna chandra <poornanaidu1@gmail.com>
2025-01-22 12:40:45 +0530

    changes

--- vars.tf
+++ vars.tf
@@ -1,6 +1,13 @@
-variable "vpc" {}     #this is variable for module in main.tf in roboshop-terraform-v1 and we are sending to main.tf module
+variable "vpc" {}
 variable "default_vpc_id" {}
 variable "default_vpc_cidr" {}
 variable "default_vpc_route_table_id" {}
 variable "tags" {}
 variable "env" {}
+variable "alb" {}
+variable "docdb" {}
+variable "rds" {}
+variable "elasticache" {}
+variable "rabbitmq" {}
+variable "zone_id" {}
+variable "apps" {}
