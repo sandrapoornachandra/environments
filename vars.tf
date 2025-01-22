3e20f5ef2716e95d412ccc9d3affbc420032e536
s poorna chandra <poornanaidu1@gmail.com>
2025-01-22 13:06:02 +0530

    changes

--- vars.tf
+++ vars.tf
@@ -4,10 +4,10 @@ variable "default_vpc_cidr" {}
 variable "default_vpc_route_table_id" {}
 variable "tags" {}
 variable "env" {}
-variable "alb" {}
-variable "docdb" {}
-variable "rds" {}
-variable "elasticache" {}
-variable "rabbitmq" {}
-variable "zone_id" {}
-variable "apps" {}
+# variable "alb" {}
+# variable "docdb" {}
+# variable "rds" {}
+# variable "elasticache" {}
+# variable "rabbitmq" {}
+# variable "zone_id" {}
+# variable "apps" {}
