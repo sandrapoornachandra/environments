fe239f7d372c28cc8a325a3281d2452af0be744b
s poorna chandra <poornanaidu1@gmail.com>
2025-01-22 12:17:52 +0530

    dev environment

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source                     = "git::https://github.com/SPOORNACHANDRA/practice-tf-module-vpc.git"
+  source                     = "git::https://github.com/SPOORNACHANDRA/vpc.git"
   for_each                   = var.vpc      #this is for how many times i have to iterate
   cidr                       = each.value["cidr"]
   subnets                    = each.value["subnets"]
